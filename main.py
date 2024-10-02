052aa6fe5658563e1bfd75dc327942eebea0c72e
iriasu <iriasu159@gmail.com>
2024-10-02 13:04:22 +0100

    create player object

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from constants import *
 import pygame
-
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt=0
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((10,10,10))
+        player.draw(screen)
         pygame.display.flip()
-
+        dt=clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
